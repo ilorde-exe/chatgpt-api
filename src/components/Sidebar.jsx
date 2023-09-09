da81bb28558f62e2b0b988020132ac5df0bf8ed5
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2023-09-10 01:33:07 +0530

    Chat bot logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 function Sidebar() {
   return (
-    <div className="flex basis-1/4 flex-col p-2 h-screen bg-gray-900">
-      <h3 className="text-2xl smd:text-4xl xl:text-3xl font-semibold mb-1 py-1 smd:mb-2 text-center text-white">
-        AI Chatbot
-      </h3>
+    <div className="flex items-center basis-1/4 flex-col p-2 h-screen bg-gray-900">
+      <div className="flex space-x-2">
+        <img src="src\assets\logo.svg" alt="ayo broken" className=" h-12" />
+        <h3 className="text-2xl smd:text-4xl xl:text-3xl font-semibold mb-1 py-1 smd:mb-2 text-center text-white">
+          AI Chatbot
+        </h3>
+      </div>
       <div>{/*New chat button */}</div>
       <div>{/*All chats*/}</div>
     </div>
