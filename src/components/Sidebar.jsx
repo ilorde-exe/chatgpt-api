7880ead08a849d9aad0a9aaa2321be7c775320b9
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2023-09-14 21:45:15 +0530

    Trying to connect api
    Co-Authored-By: Pranavd500 <114133861+Pranavd500@users.noreply.github.com>

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,13 +18,13 @@ function Sidebar() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="2"
+              strokeWidth="2"
               stroke="#111827"
               class="w-6 h-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M12 4.5v15m7.5-7.5h-15"
               />
             </svg>
